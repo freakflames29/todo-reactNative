bd0095bc9c458ad8eef60f3c0fa9d5915e37b451
Sourav Das <iamsouravdaswade420@gmail.com>
2024-08-19 18:39:07 +0530

    added modal

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View, TextInput, Alert,ScrollView } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Alert,
+  ScrollView,
+  FlatList,
+  Modal,
+} from "react-native";
 import { useState } from "react";
 import TempComp from "./TempComp";
 // import styles from "./style";
-
+import GoalItem from "./components/GoalItem";
 export default function App() {
   const [enteredText, setEnteredText] = useState("");
   const [goals, setGoals] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
 
   function enteredTextHandler(theText) {
     setEnteredText(theText);
@@ -15,31 +26,78 @@ export default function App() {
   function buttonClickHandler() {
     console.log(enteredText);
     setGoals((prevGoals) => [...prevGoals, enteredText]);
-    // setEnteredText("");
+    setEnteredText("");
+    modalToggle();
     // Alert.alert("added");
+  }
+
+  function DeleteHandler(index) {
+    Alert.alert("Delete goal", "Do you want to delete the goal? ", [
+      {
+        text: "Cancel",
+        onPress: () => {
+          console.log("Cancel pressed");
+        },
+        style: "cancel",
+      },
+      { text: "Yeah Bro!!", onPress: () => console.log("OK Pressed") },
+    ]);
+
+    console.log(goals + " => DELETED");
+    setGoals((prevGoals) => {
+      return prevGoals.filter((goal, ind) => ind !== index);
+    });
+  }
+
+  function modalToggle() {
+    setModalVisible((prev) => !prev);
   }
 
   return (
     <View style={styles.container}>
-      <View style={styles.GoalView}>
-        <TextInput
-          placeholder="Enter Goal now"
-          style={styles.TextBox}
-          onChangeText={enteredTextHandler}
-          // value={enteredText}
-        />
+      <Button title="Add a new goal" onPress={modalToggle} />
+      <Modal visible={modalVisible} animationType="slide">
+        <View style={styles.GoalView}>
+          <TextInput
+            placeholder="Enter Goal now"
+            style={styles.TextBox}
+            onChangeText={enteredTextHandler}
+            value={enteredText}
+          />
 
-        <Button title="Add Goal" onPress={buttonClickHandler} />
-      </View>
-
+          <View style={styles.buttonViews}>
+            <View style={styles.Button}>
+              <Button title="Cancel" />
+            </View>
+            <View style={styles.Button}>
+              <Button title="Add Goal" onPress={buttonClickHandler} />
+            </View>
+          </View>
+        </View>
+      </Modal>
       <View style={styles.listGoals}>
-      <ScrollView>
+        {/* <ScrollView>
           {goals.map((goal, index) => (
             <View key={index} style={styles.listItems}>
               <Text style={styles.listText}>{goal}</Text>
             </View>
           ))}
-        </ScrollView>
+        </ScrollView> */}
+
+        {/* a good way to list cause it is dynamic kind of lazyLoad  */}
+
+        <FlatList
+          data={goals}
+          renderItem={(itemData) => {
+            return (
+              <GoalItem
+                text={itemData.item}
+                keyValue={itemData.index}
+                onDeleteTap={DeleteHandler}
+              />
+            );
+          }}
+        />
       </View>
     </View>
   );
@@ -53,32 +111,32 @@ const styles = StyleSheet.create({
   },
   GoalView: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
+    // flexDirection: "row",
+    justifyContent: "center",
     alignItems: "center",
     borderBottomWidth: 1,
-    marginBottom: 24,
+    // marginBottom: 24,
+  },
+  buttonViews: {
+    marginVertical: 20,
+    flexDirection: "row",
+  },
+  Button:{
+    marginHorizontal:10,
+    width:"30%"
+
+
   },
   TextBox: {
     borderWidth: 2,
     borderColor: "#ecc21c",
-    width: "70%",
-    borderRadius: 15,
+    width: "80%",
+    borderRadius: 10,
     padding: 10,
     marginRight: 10,
     fontSize: 20,
   },
   listGoals: {
     flex: 7,
-  },
-  listItems: {
-    margin: 5,
-    padding: 10,
-    backgroundColor: "#f3726b",
-    borderRadius: 5,
-  },
-  listText: {
-    fontSize: 20,
-    textTransform: "capitalize",
   },
 });
---
+++ components/GoalItem.js
@@ -0,0 +1,25 @@
+import { StyleSheet,View,Text,Pressable} from "react-native";
+const GoalItem = (props) => {
+  return (
+    <Pressable onPress={props.onDeleteTap.bind(this,props.keyValue)}>
+        <View key= {props.keyValue} style={styles.listItems} >
+        <Text style={styles.listText}>{props.text}</Text>
+        </View>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+
+  listItems: {
+    margin: 5,
+    padding: 10,
+    backgroundColor: "#f3726b",
+    borderRadius: 5,
+  },
+  listText: {
+    fontSize: 20,
+    textTransform: "capitalize",
+  },
+})
+export default GoalItem;
